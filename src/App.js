f392dde242c6b71e78739503656396cd304aa215
AnnaSilverIT <student291297@gmail.com>
2025-03-25 16:39:23 +0300

    add piechart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Chart } from "react-google-charts"
+
+
 
 function App() {
+  const data = [
+    ["Задача", "Часов в день"],
+    ["Работа", 7],
+    ["Еда", 2],
+    ["Обучение", 2],
+    ["Общение с семьей", 4],
+    ["Сон", 9],
+  ];
+
+  const options = {
+    title: "Моя дневная активность",
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
       </header>
     </div>
   );
 }
+
 
 export default App;
